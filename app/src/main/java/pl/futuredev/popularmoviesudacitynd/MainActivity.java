cdde54b9b20cfa2b50ca18879d9b740fac73fda0
Wiktor Kalinowski <Wiktorl4z@users.noreply.github.com>
2018-02-25 21:27:28 +0100

    Init

---
+++ app/src/main/java/pl/futuredev/popularmoviesudacitynd/MainActivity.java
@@ -0,0 +1,39 @@
+package pl.futuredev.popularmoviesudacitynd;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String API_KEY = BuildConfig.API_KEY;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.top_rated:
+                return true;
+            case R.id.popular:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
